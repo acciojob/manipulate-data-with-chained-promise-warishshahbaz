8df6e115cc2f75f098bf3b7a47eb997bed29547f
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-16 00:06:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+
+	let promise1 =(arr)=>{
+		return new Promise((res,rej)=>{
+		setTimeout(()=>{
+			let res = arr.filter(item => item%2 == 0);
+			res(res);
+		},1000)
+	})
+	} 
+
+
+	let promise2 =(data)=>{
+		new Promise((res,rej)=>{
+		setTimeout(()=>{
+			let res = arr.filter(item => item%2 == 0);
+			res(res);
+		},1000)
+	})
+	} 
+promise1().than((data)=>{
+	console.log(data)
+	return promise2(data);
+}).than((data)=>{
+	console.log(data)
+})
+
